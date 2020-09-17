4f61dc563eb148697de5b7d8c8feac3e84b31bf9
liyuan xu <>
2020-09-17 16:56:05 +0900

    implement generate train data code

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,4 @@ from .acme_utils import load_rl_unplugged_dataset
 from .acme_utils import load_offline_dm_control_dataset
 from .acme_utils import load_offline_bsuite_dataset
 from .evaluation import ope_evaluation
+from .generate_data import generate_train_data
