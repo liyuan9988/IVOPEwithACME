c10a702c04a383cd11fc2da9a8fefeb0325b9347
Yutian Chen <yutianc@google.com>
2020-09-27 17:13:40 +0100

    Finishes the implementation of deepiv.

--- src/ope/deepiv/nn_structure/__init__.py
+++ src/ope/deepiv/nn_structure/__init__.py
@@ -3,15 +3,18 @@ from acme.tf import utils as tf2_utils
 import sonnet as snt
 from typing import Tuple
 from .cartpole_swingup import make_value_func_cartpole
-from .bsuite_cartpole_swingup import bsuite_make_value_func_cartpole
+from .bsuite_network import bsuite_make_value_func
 
 
-def make_ope_networks(task_id: str, environment_spec: EnvironmentSpec) -> Tuple[snt.Module, snt.Module]:
+def make_ope_networks(task_id: str,
+                      environment_spec: EnvironmentSpec,
+                      num_cat: int) -> Tuple[snt.Module, snt.Module]:
 
     if task_id == "cartpole_swingup" or  task_id == "dm_control_cartpole_swingup":
         value_func, mixture_density = make_value_func_cartpole()
-    elif task_id == "bsuite_cartpole_swingup":
-        value_func, mixture_density = bsuite_make_value_func_cartpole()
+    elif task_id.startswith("bsuite"):
+        value_func, mixture_density = bsuite_make_value_func(
+            environment_spec, num_cat)
     else:
         raise ValueError(f"task id {task_id} not known")
 
