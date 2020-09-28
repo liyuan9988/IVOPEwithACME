79e1eaa1b087e1d17467ded0cf2e3156409a848a
Yutian Chen <yutianc@google.com>
2020-09-28 16:55:52 +0100

    Simply code in deepiv, smaller predictor net. DFIV uses d_tm1.

--- src/ope/deepiv/nn_structure/bsuite_network.py
+++ src/ope/deepiv/nn_structure/bsuite_network.py
@@ -17,15 +17,8 @@ class MixtureDensity(snt.Module):
         self.obs_size = np.prod(self.obs_shape)
         self.num_cat = num_cat
 
-        # self._net = snt.Sequential([networks.LayerNormMLP([1024, 1024, 512], activate_final=True)])
-        # self._logits = snt.Linear(num_cat)
-        # self._discount_logits = snt.Linear(1)
-        # self._locs_weight = tf.Variable(tf.random.normal([hiddens[-1], num_cat, obs_size]))
-        # self._locs_bias = tf.Variable(tf.random.normal([num_cat, obs_size]))
-        # self._scale_weight = tf.Variable(tf.random.normal([[hiddens[-1], num_cat, obs_size]))
-        # self._scale_bias = tf.Variable(tf.random.normal([num_cat, obs_size]))
-
-        self._net = snt.nets.MLP([150, 100, 50], activate_final=True)
+        # self._net = snt.nets.MLP([150, 100, 50], activate_final=True)
+        self._net = snt.nets.MLP([50, 50], activate_final=True)
         self._discount_logits = snt.Linear(1)
         self._mix_logits = snt.Linear(num_cat)
         self._locs = snt.Linear(num_cat * self.obs_size)
@@ -42,8 +35,6 @@ class MixtureDensity(snt.Module):
         mix_logits = self._mix_logits(feature)
         locs = tf.reshape(self._locs(feature), [-1, self.num_cat, self.obs_size])
         scales = tf.exp(tf.reshape(self._scales(feature), [-1, self.num_cat, self.obs_size]))
-        # locs = tf.einsum("ij,jkl->ikl", feature, self._locs_weight) + self._locs_bias
-        # scales = tf.exp(tf.einsum("ij,jkl->ikl", feature, self._scale_weight) + self._scale_bias)
         obs_distr = tfd.MixtureSameFamily(mixture_distribution=tfd.Categorical(logits=mix_logits),
                                           components_distribution=tfd.MultivariateNormalDiag(loc=locs,
                                                                                              scale_diag=scales)
@@ -51,28 +42,6 @@ class MixtureDensity(snt.Module):
         discount_logits = self._discount_logits(feature)
         discount_distr = tfd.Bernoulli(logits=discount_logits)
         return obs_distr, discount_distr
-
-    def obtain_sampled_value_function(self, current_obs, action, policy, value_func):
-        # sampled_next_obs = self.__call__(current_obs, action).sample()
-        # sampled_next_obs = tf.reshape(sampled_next_obs, (-1,) + self.obs_shape)
-        # sampled_action = policy(sampled_next_obs)
-        # next_value = value_func(sampled_next_obs, sampled_action)
-        obs_distr, discount_distr = self.__call__(current_obs, action)
-        sampled_next_obs = obs_distr.sample()
-        sampled_next_obs = tf.reshape(sampled_next_obs, (-1,) + self.obs_shape)
-
-        sampled_action = policy(sampled_next_obs)
-
-        sampled_discount = discount_distr.sample()
-
-        value = value_func(sampled_next_obs, sampled_action)
-
-        if sampled_discount.shape != value.shape:
-            raise ValueError(
-                f'Unmatched shape sampled_discount.shape '
-                f'({sampled_discount.shape}) != value.shape ({value.shape})')
-        next_value = sampled_discount * value
-        return next_value
 
 
 class ValueFunction(snt.Module):
--- src/ope/dfiv/learner.py
+++ src/ope/dfiv/learner.py
@@ -1,4 +1,5 @@
 # Lint as: python3
+# pylint: disable=bad-indentation,line-too-long
 """DFIV Learner implementation."""
 
 from typing import Dict, List
@@ -40,6 +41,7 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
                  value_iter: int,
                  dataset: tf.data.Dataset,
                  ignore_terminal: bool = False,
+                 d_tm1_weight: float = 1.0,
                  counter: counting.Counter = None,
                  logger: loggers.Logger = None,
                  checkpoint: bool = True):
@@ -74,6 +76,7 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
         self.value_reg = value_reg
         self.instrumental_reg = instrumental_reg
         self.ignore_terminal = ignore_terminal
+        self.d_tm1_weight = d_tm1_weight
 
         # Get an iterator over the dataset.
         self._iterator = iter(dataset)  # pytype: disable=wrong-arg-types
@@ -115,8 +118,8 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
 
         for i in range(self.value_iter):
             for j in range(self.instrumental_iter // self.value_iter):
-                o_tm1, a_tm1, r_t, d_t, o_t = self.stage1_input[:5]
-                stage1_loss = self.update_instrumental(o_tm1, a_tm1, r_t, d_t, o_t)
+                o_tm1, a_tm1, r_t, d_t, o_t, _, d_tm1 = self.stage1_input[:7]
+                stage1_loss = self.update_instrumental(o_tm1, a_tm1, r_t, d_t, o_t, d_tm1)
 
             stage2_loss = self.update_value(self.stage1_input, self.stage2_input)
 
@@ -124,20 +127,22 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
         self._num_steps.assign_add(1)
 
         # Compute the global norm of the gradients for logging.
-        fetches = {'stage1_loss': stage1_loss, "stage2_loss": stage2_loss}
+        fetches = {'stage1_loss': stage1_loss, 'stage2_loss': stage2_loss}
 
         return fetches
 
-    def update_instrumental(self, current_obs, action, reward, discount, next_obs):
+    def update_instrumental(self, current_obs, action, reward, discount, next_obs, d_tm1):
         next_action = self.policy(next_obs)
         discount = tf.expand_dims(discount, axis=1)
+        d_tm1 = tf.expand_dims(d_tm1, axis=1)
         # target = discount * self.value_feature(next_obs, next_action, training=False)
-        target = discount * add_const_col(self.value_feature(next_obs, next_action, training=False))
+        target = d_tm1 * discount * add_const_col(self.value_feature(next_obs, next_action, training=False))
         l2 = snt.regularizers.L2(self.instrumental_reg)
         with tf.GradientTape() as tape:
             feature = self.instrumental_feature(obs=current_obs, action=action, training=True)
             if self.ignore_terminal:
                 feature = discount * feature
+            feature = d_tm1 * feature
             loss = linear_reg_loss(target, feature, self.stage1_reg)
             loss = loss + l2(self.instrumental_feature.trainable_variables)
             loss /= current_obs.shape[0]
@@ -148,27 +153,34 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
         return loss
 
     def update_value(self, stage1_input, stage2_input):
-        current_obs_1st, action_1st, reward_1st, discount_1st, next_obs_1st = stage1_input[:5]
-        current_obs_2nd, action_2nd, reward_2nd = stage2_input[:3]
+        current_obs_1st, action_1st, _, discount_1st, next_obs_1st, _, d_tm1_1st = stage1_input[:7]
+        current_obs_2nd, action_2nd, reward_2nd, _, _, _, d_tm1_2nd = stage2_input[:7]
         next_action_1st = self.policy(next_obs_1st)
         discount_1st = tf.expand_dims(discount_1st, axis=1)
+        d_tm1_1st = tf.expand_dims(d_tm1_1st, axis=1)
+        d_tm1_2nd = tf.expand_dims(d_tm1_2nd, axis=1)
 
         instrumental_feature_1st = self.instrumental_feature(obs=current_obs_1st, action=action_1st,
                                                              training=False)
         if self.ignore_terminal:
             instrumental_feature_1st = discount_1st * instrumental_feature_1st
+        instrumental_feature_1st = d_tm1_1st * instrumental_feature_1st
         instrumental_feature_2nd = self.instrumental_feature(obs=current_obs_2nd, action=action_2nd,
                                                              training=False)
         l2 = snt.regularizers.L2(self.value_reg)
         with tf.GradientTape() as tape:
             # target_1st = discount_1st * self.value_feature(obs=next_obs_1st, action=next_action_1st, training=True)
-            target_1st = discount_1st * add_const_col(self.value_feature(obs=next_obs_1st, action=next_action_1st, training=True))
+            target_1st = d_tm1_1st * discount_1st * add_const_col(self.value_feature(obs=next_obs_1st, action=next_action_1st, training=True))
             stage1_weight = fit_linear(target_1st, instrumental_feature_1st, self.stage1_reg)
             predicted_feature = linear_reg_pred(instrumental_feature_2nd, stage1_weight)
             # current_feature = self.value_feature(obs=current_obs_2nd, action=action_2nd, training=True)
             current_feature = add_const_col(self.value_feature(obs=current_obs_2nd, action=action_2nd, training=True))
-            predicted_feature = current_feature - self.discount * predicted_feature
-            loss = linear_reg_loss(tf.expand_dims(reward_2nd, -1), predicted_feature, self.stage2_reg)
+            predicted_feature = current_feature - d_tm1_2nd * self.discount * predicted_feature
+            # loss = linear_reg_loss(tf.expand_dims(reward_2nd, -1), predicted_feature, self.stage2_reg)
+
+            weight = d_tm1_2nd + (1.0 - d_tm1_2nd) * tf.convert_to_tensor(self.d_tm1_weight, dtype=tf.float32)
+            loss = linear_reg_loss(weight * tf.expand_dims(reward_2nd, -1), weight * predicted_feature, self.stage2_reg)
+
             loss = loss + l2(self.value_feature.trainable_variables)
             loss /= current_obs_2nd.shape[0]
 
@@ -177,28 +189,36 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
         return loss
 
     def update_final_weight(self, stage1_input, stage2_input):
-        current_obs_1st, action_1st, reward_1st, discount_1st, next_obs_1st = stage1_input[:5]
-        current_obs_2nd, action_2nd, reward_2nd = stage2_input[:3]
+        current_obs_1st, action_1st, _, discount_1st, next_obs_1st, _, d_tm1_1st = stage1_input[:7]
+        current_obs_2nd, action_2nd, reward_2nd, _, _, _, d_tm1_2nd = stage2_input[:7]
         next_action_1st = self.policy(next_obs_1st)
         discount_1st = tf.expand_dims(discount_1st, axis=1)
+        d_tm1_1st = tf.expand_dims(d_tm1_1st, axis=1)
+        d_tm1_2nd = tf.expand_dims(d_tm1_2nd, axis=1)
 
         instrumental_feature_1st = self.instrumental_feature(obs=current_obs_1st, action=action_1st,
                                                              training=False)
         if self.ignore_terminal:
             instrumental_feature_1st = discount_1st * instrumental_feature_1st
+        instrumental_feature_1st = d_tm1_1st * instrumental_feature_1st
         instrumental_feature_2nd = self.instrumental_feature(obs=current_obs_2nd, action=action_2nd,
                                                              training=False)
 
         # target_1st = discount_1st * self.value_feature(obs=next_obs_1st, action=next_action_1st, training=False)
-        target_1st = discount_1st * add_const_col(self.value_feature(obs=next_obs_1st, action=next_action_1st, training=False))
+        target_1st = d_tm1_1st * discount_1st * add_const_col(self.value_feature(obs=next_obs_1st, action=next_action_1st, training=False))
         stage1_weight = fit_linear(target_1st, instrumental_feature_1st, self.stage1_reg)
         predicted_feature = linear_reg_pred(instrumental_feature_2nd, stage1_weight)
         # current_feature = self.value_feature(obs=current_obs_2nd, action=action_2nd, training=False)
         current_feature = add_const_col(self.value_feature(obs=current_obs_2nd, action=action_2nd, training=False))
         # predicted_feature = add_const_col(current_feature) - self.discount * add_const_col(predicted_feature)
-        predicted_feature = current_feature - self.discount * predicted_feature
+        predicted_feature = current_feature - d_tm1_2nd * self.discount * predicted_feature
+        # self.value_func._weight.assign(
+        #     fit_linear(tf.expand_dims(reward_2nd, -1), predicted_feature, self.stage2_reg))
+
+        weight = d_tm1_2nd + (1.0 - d_tm1_2nd) * tf.convert_to_tensor(self.d_tm1_weight, dtype=tf.float32)
         self.value_func._weight.assign(
-            fit_linear(tf.expand_dims(reward_2nd, -1), predicted_feature, self.stage2_reg))
+            fit_linear(weight * tf.expand_dims(reward_2nd, -1), weight * predicted_feature, self.stage2_reg))
+
 
     def step(self):
         # Do a batch of SGD.
@@ -211,7 +231,8 @@ class DFIVLearner(acme.Learner, tf2_savers.TFSaveable):
         # Snapshot and attempt to write logs.
         if self._snapshotter is not None:
             self._snapshotter.save()
-        self._logger.write(result)
+        if self._num_steps % 10 == 0:
+            self._logger.write(result)
 
     def get_variables(self, names: List[str]) -> List[np.ndarray]:
         return tf2_utils.to_numpy(self._variables)
