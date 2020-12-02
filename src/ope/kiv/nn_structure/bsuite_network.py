8c2444393e91ef0849f018e8bc53de34856ef0bd
liyuan xu <>
2020-12-02 22:17:48 +0900

    Revert "add automatic bandwidth selection"
    This reverts commit 95ccc14dbef4ef5dcbc127fa780057cbe2ab03ce.

--- src/ope/kiv/nn_structure/bsuite_network.py
+++ src/ope/kiv/nn_structure/bsuite_network.py
@@ -5,17 +5,8 @@ from acme.tf import utils as tf2_utils
 from acme.tf import networks
 from acme.specs import EnvironmentSpec
 import numpy as np
-from scipy.spatial.distance import cdist
 
 from src.utils.tf_linear_reg_utils import outer_prod, add_const_col
-
-
-def get_median(X):
-    A = X.numpy()
-    dist_mat = cdist(A, A, "sqeuclidean")
-    res: float = np.median(dist_mat)
-    return res
-
 
 @snt.allow_empty_variables
 class RandomFourierFeature(snt.Module):
@@ -38,59 +29,40 @@ class RandomFourierFeature(snt.Module):
         z_vec = tf.cos(tf.matmul(x, self.random_weights_) + self.random_offset_) / np.sqrt(self.n_components)
         return z_vec
 
-
 @snt.allow_empty_variables
 class InstrumentalFeature(snt.Module):
 
-    def __init__(self, environment_spec, n_component, gamma=None):
+    def __init__(self, environment_spec, n_component, gamma):
         super(InstrumentalFeature, self).__init__()
-        self.rff = None
-        self.n_component = n_component
-        self.gamma = None
+        self.rff = RandomFourierFeature(n_component=n_component, gamma=gamma)
         self.n_action = environment_spec.actions.num_values
         self.flat = snt.Flatten()
-
-    @snt.once
-    def _initialize(self, x):
-        if self.gamma is None:
-            self.gamma = 1.0 / get_median(x)
-        self._net = RandomFourierFeature(n_component=self.n_component, gamma=self.gamma)
 
     def __call__(self, obs, action):
         action_aug = tf.one_hot(action, depth=self.n_action)
         inputs = tf.concat([self.flat(obs), action_aug], axis=1)
-        self._initialize(inputs)
         return self.rff(inputs)
 
 
 @snt.allow_empty_variables
 class ValueFeature(snt.Module):
 
-    def __init__(self, environment_spec, n_component, gamma=None):
+    def __init__(self, environment_spec, n_component, gamma):
         super(ValueFeature, self).__init__()
-        self._net = None
-        self.gamma = gamma
-        self.n_component = n_component
+        self._net = RandomFourierFeature(n_component=n_component, gamma=gamma)
         self.n_action = environment_spec.actions.num_values
         self.flat = snt.Flatten()
-
-    @snt.once
-    def _initialize(self, x):
-        if self.gamma is None:
-            self.gamma = 1.0 / get_median(x)
-        self._net = RandomFourierFeature(n_component=self.n_component, gamma=self.gamma)
 
     def __call__(self, obs, action):
         action_aug = tf.one_hot(action, depth=self.n_action)
         inputs = tf.concat([self.flat(obs), action_aug], axis=1)
-        self._initialize(inputs)
         return self._net(inputs)
 
 
 @snt.allow_empty_variables
 class ValueFunction(snt.Module):
 
-    def __init__(self, environment_spec, n_component, gamma=None):
+    def __init__(self, environment_spec, n_component, gamma):
         super(ValueFunction, self).__init__()
         self._feature = ValueFeature(environment_spec, n_component=n_component, gamma=gamma)
         self.n_action = environment_spec.actions.num_values
@@ -100,7 +72,7 @@ class ValueFunction(snt.Module):
         return tf.matmul(self._feature(obs, action), self._weight)
 
 
-def make_value_func_bsuite(environment_spec, n_component=100, gamma=None) -> Tuple[snt.Module, snt.Module]:
+def make_value_func_bsuite(environment_spec, n_component=100, gamma=10.0) -> Tuple[snt.Module, snt.Module]:
     value_function = ValueFunction(environment_spec, n_component=n_component, gamma=gamma)
     instrumental_feature = InstrumentalFeature(environment_spec, n_component=n_component, gamma=gamma)
     return value_function, instrumental_feature
